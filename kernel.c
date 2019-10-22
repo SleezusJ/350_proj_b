379b19a974862672ea537a209542a056de60def6
Kyle <k1snow@student.bridgew.edu>
2019-10-22 18:32:48 -0400

    readString() updooted
    see title

--- kernel.c
+++ kernel.c
@@ -33,14 +33,30 @@ void printString(char* chars){
 char* readString(char line[]){
 	char new;
 	do{
+
 		new = interrupt(0x16,0,0,0,0);
+		
+		if(new == 0x08 && *line != 0){//---------------------
+			interrupt(0x10,0xe*256+0x08,0,0,0);//print bkspc
+			line--;
+			interrupt(0x10,0xe*256+' ',0,0,0);//print space over bkspc 
+			interrupt(0x10,0xe*256+0x08,0,0,0);//second backspace??
+				
+		}
+		else{
 		*line = new;
 		interrupt(0x10,0xe*256+*line,0,0,0);
 		line++;
+		}
 
 	}while(new != 0xd);
 
-	*line = 0x0; //append NULL terminator (Don't know if we have to do this btw)
+
+	*line = 0xa; //line return
+	line++;
+	*line = 0x10;
+	line++;
+	*line = 0x0; //append NULL
 
 	return line;
 }
